5d3e3ef1d3728a80d859f1638521055b2104d1a8
Hasan <syedhasanali931@gmail.com>
2025-08-01 18:50:20 +0530

    business Schema updated

--- models/Business.js
+++ models/Business.js
@@ -2,9 +2,11 @@ const mongoose = require('mongoose');
 
 const businessSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  sector: String,
+  sector: {type : String, enum : ["Manufacturing", "Retail", "Agriculture", "Health Care", "Education", "Transportation", "Finance", "Construction", "Other"], required : true},
   valuation: Number,
-  description: String,
+  constitution :  {type : String, enum : [ "Limited Liability Partnership (LLP)", "Public Limited Company", "Private Limited Company", "Sole Proprietorship","Other"], required : true},
+  revenue: {type : String, enum : ["upto 50 Lacs", "50lac-1Crore", "1Crore to 5cr", "5Cr to 20 Cr", "Above 20 Cr"], required : true},
+  companySize: {type : String, enum : ["1-10 Employees", "11-50 Employees", "51-100 Employees", "101-500 Employees", "501-1000 Employees", "1001-5000 Employees", "5001-10000 Employees", "Above 10000 Employees"], required : true},
   ownerEmail: String,
   createdAt: { type: Date, default: Date.now }
 });
